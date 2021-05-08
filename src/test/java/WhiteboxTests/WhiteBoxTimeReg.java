84cc7109041eeb0e6054e27484e83e23f49ae7ab
madskost <mads@zoie.dk>
2021-05-08 21:05:46 +0200

    update whiteboxtimereg

--- src/test/java/WhiteboxTests/WhiteBoxTimeReg.java
+++ src/test/java/WhiteboxTests/WhiteBoxTimeReg.java
@@ -7,9 +7,23 @@ import org.junit.jupiter.api.Test;
 import system.app.Activity;
 import system.app.Employee;
 import system.app.PKV;
+import system.app.Pomodoro;
 import system.app.Project;
 
 class WhiteBoxTimeReg {
+	
+//	public void addTime(Employee employee, int i) throws Exception {
+//		boolean found = false;
+//		for (Pomodoro pomodoro : pomodoros) {
+//			if (pomodoro.getEmployee() == employee) {   //1
+//				pomodoro.addTime(i);                    //1i
+//				found=true;
+//			}
+//		}
+//		if (!found) {                                   //2
+//			throw new Exception("You are not assigned to this activity");
+//		}
+//	}
 
 	PKV pkvSystem;
 	Employee eUser;
@@ -25,7 +39,7 @@ class WhiteBoxTimeReg {
 	}
 	
 	@Test
-	void path1() {
+	void positiveArg() {
 		assertTrue(selectedActivty.getTotalTime() == 0);
 		try {
 			selectedActivty.addTime(eUser, 10);
@@ -51,6 +65,51 @@ class WhiteBoxTimeReg {
 			} catch (Exception e) {}
 		}
 		assertTrue(selectedActivty.getTotalTime() == 5050);
+	}
+	
+	@Test
+	void notAssigned() throws Exception{
+		selectedProject = pkvSystem.getProject("ABCE");
+		selectedActivty = selectedProject.getActivity("Sick Days");
+		
+		assertTrue(selectedActivty.getTotalTime() == 0);
+		
+		try {
+			selectedActivty.addTime(eUser, 21);
+		} catch (Exception e) {}
+		
+		assertTrue(selectedActivty.getTotalTime() == 0);
+	}
+	
+	@Test
+	void newAssigned() throws Exception {
+		Employee eUser2 = pkvSystem.getEmployee("ABCE");
+		selectedActivty.addEmployee(eUser2);
+		
+		assertTrue(selectedActivty.getTotalTime() == 0);
+		
+		try {
+			selectedActivty.addTime(eUser2, 123);
+		} catch (Exception e) {}
+		
+		assertTrue(selectedActivty.getTotalTime() == 123);
+	}
+	
+	@Test
+	void multipleAssigned() throws Exception {
+		Employee eUser2 = pkvSystem.getEmployee("ABCE");
+		selectedActivty.addEmployee(eUser2);
+		try {
+			selectedActivty.addTime(eUser, 1);
+		} catch (Exception e) {}
+		
+		assertTrue(selectedActivty.getTotalTime() == 1);
+		
+		try {
+			selectedActivty.addTime(eUser2, 10);
+		} catch (Exception e) {}
+		
+		assertTrue(selectedActivty.getTotalTime() == 11);
 	}
 
 }
