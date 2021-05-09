ea8e529dfcbfb0437ccde162e91abfbd483f7979
unknown <s204166@student.dtu.dk>
2021-05-09 12:43:52 +0200

    improved whitebox test for deleteproject

--- src/test/java/WhiteboxTests/WhiteBoxDeleteProject.java
+++ src/test/java/WhiteboxTests/WhiteBoxDeleteProject.java
@@ -1,24 +1,22 @@
 package WhiteboxTests;
 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
 import system.app.Employee;
 import system.app.PKV;
 
-public class DeleteProjectWhiteBox {
+public class WhiteBoxDeleteProject {
 	PKV system;
 	Employee employee;
 
-	public DeleteProjectWhiteBox() throws Exception {
+	public WhiteBoxDeleteProject() throws Exception {
 		system = new PKV();
 		system.login("ABCD");
 		employee = system.getEmployee("ABCD");
 	}
-	
 	@Test
 	void deleteSuccessfully() throws Exception {
 		system.createProject("FailProject");
@@ -26,35 +24,18 @@ public class DeleteProjectWhiteBox {
 		system.getProject("FailProject").setLeader(employee);
 		system.deleteProject("FailProject");
 		assertFalse(system.hasProject("FailProject"));
-
 	}
 	@Test
 	void deleteProjectThatdoesNotExisError() throws Exception {
 		system.createProject("FailProject");
 		system.setSelectedProject(system.getProject("FailProject"));
 		system.getProject("FailProject").setLeader(employee);
-		try {
-			system.deleteProject("#NOT#FailProject");
-		    Assert.fail("Failure in DeleteProjectWhiteBox: Cannot delete project that does not exist");
-		} 
-		catch (Exception e) {
-		    String expectedException = "A project with that name doesn't exits";
-		    Assert.assertEquals(expectedException, e.getMessage());
-		}
+		assertThrows(Exception.class, () -> system.deleteProject("#NOT#FailProject"));
 	}
 	@Test
 	void deleteProjectWhileNotBeingLeaderError() throws Exception {
 		system.createProject("FailProject");
 		system.setSelectedProject(system.getProject("FailProject"));
-		try {
-			system.deleteProject("FailProject");
-		    Assert.fail("Failure in DeleteProjectWhiteBox: Cannot delete project if employee is not the project leader");
-		} 
-		catch (Exception e) {
-		    String expectedException = "Only the project leaders can delete the project";
-		    Assert.assertEquals(expectedException, e.getMessage() );
-		}
-	}
-	
-	
+		assertThrows(Exception.class, () -> system.deleteProject("FailProject"));
+	}	
 }
