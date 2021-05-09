f59f87fb6040e3dbd06eb93f9ba834952cd66565
Maximilian <latityde314@gmail.com>
2021-05-09 20:57:29 +0200

    Whitebox maxi

---
+++ src/test/java/WhiteboxTests/WhiteBoxRemoveEmployee.java
@@ -0,0 +1,47 @@
+package WhiteboxTests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import system.app.Activity;
+import system.app.Employee;
+import system.app.PKV;
+
+public class WhiteBoxRemoveEmployee {
+	PKV pkv;
+	Employee employee;
+	Activity activity;
+
+	public WhiteBoxRemoveEmployee() throws Exception {
+		pkv = new PKV();
+		pkv.login("ABCD");
+		employee = pkv.getEmployee("ABCD");
+		pkv.createProject("TestProject");
+		pkv.setSelectedProject(pkv.getProject("TestProject"));
+		pkv.getSelectedProject().setLeader(employee);
+		pkv.getSelectedProject().createActivty(employee, "TestActivity");
+		pkv.setSelectedActivity(pkv.getSelectedProject().getActivity("TestActivity"));
+	}
+	@Test
+	void removeEmployeeSucessfully() throws Exception {
+		assertFalse(pkv.getSelectedActivity().hasEmployee(employee));
+		pkv.getSelectedActivity().addEmployee(employee);
+		assertTrue(pkv.getSelectedActivity().hasEmployee(employee));
+	}
+	@Test
+	void removeEmployeeWhichIsNotAssigned() throws Exception {
+		assertThrows(Exception.class, () -> pkv.getSelectedActivity().removeEmployee(employee));
+		assertFalse(pkv.getSelectedActivity().hasEmployee(employee));
+	}
+	@Test
+	void removeEmployeeWhichHasTimeRegistred() throws Exception {
+		assertFalse(pkv.getSelectedActivity().hasEmployee(employee));
+		pkv.getSelectedActivity().addEmployee(employee);
+		pkv.getSelectedActivity().addTime(employee, 10);
+		assertTrue(pkv.getSelectedActivity().hasEmployee(employee));
+		assertThrows(Exception.class, () -> pkv.getSelectedActivity().removeEmployee(employee));
+	}
+}
